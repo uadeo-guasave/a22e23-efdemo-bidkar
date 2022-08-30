61cd1866fd681d267d1b2aae55ec426395bb6021
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2022-08-30 08:23:44 -0600

    Primera versión

---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+namespace EFDemo.Models
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string RememberToken { get; set; }
+    }
+}
